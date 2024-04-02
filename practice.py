e452ec4aa40335575ea0b971f2c6b272870dcc39
23anime01 <160111867+23anime01@users.noreply.github.com>
2024-04-03 00:52:04 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,5 @@
+'''
+
 num1 = float(input("Enter first number:"))
 op = input("Enter operator:")
 num2 = float(input("Enter second number:"))
@@ -12,3 +14,77 @@ elif op == "/":
     print(num1/num2)
 else:
     print("Invalid Operator")
+'''
+'''
+monthconversions = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March"
+}
+
+print(monthconversions.get("Feb"))
+'''
+'''
+#While loop concept
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+
+for letter in "Giraffe Academy":
+    print(letter)
+
+friends = ["Jim","Karen","Oscar"]
+for friend in friends:
+    print(friend)
+'''
+'''
+#Exponent function =
+
+print(2**3)
+
+def raise_to_power(base_num,pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(4,3))
+
+number_grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+
+print(number_grid[1])
+
+#Nested for loop=
+for row in number_grid:
+    for col in row:
+        print(col)
+'''
+'''
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phase: ")))
+
+#TRY EXCEPT =
+
+try:
+    number = int(input("Enter a number : "))
+    print(number)
+except:
+    print("Invalid Input")
+'''
+
